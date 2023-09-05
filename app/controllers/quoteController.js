687e7a9f5a5a66a6c35b337194acfb9d350ef28f
Hasbi Ali Firdaus <hasbialifirdaus001@gmail.com>
2023-09-05 22:57:25 +0700

    fix response api quote

--- app/controllers/quoteController.js
+++ app/controllers/quoteController.js
@@ -5,9 +5,14 @@ const Quotes = db.quote;
 const QuotesItems = db.quoteItems;
 const Op = db.Sequelize.Op;
 const logger = require("../helpers/writelog");
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
 
 // addtocart
-exports.addtocart = (req, res) => {
+exports.addtocart = async (req, res) => {
+  let token = req.session.token;
+  const payload = jwt.decode(token, config.secret);
+  // res.send(payload);
    // Validate request
    if (!req.body.productId) {
         res.status(400).send({
@@ -19,68 +24,79 @@ exports.addtocart = (req, res) => {
     const Y = today.getFullYear();
     let m = today.getMonth() + 1; // Months start at 0!
     let d = today.getDate();
-    const user = User.findByPk(req.userId);
-    const product = Product.findByPk(req.body.productId);
+    const user = await User.findOne({where:{id:payload.id}});
+    const product = await Product.findOne({where:{id:req.body.productId}});
     let result = [];
-
+    
     if (!user) {
         res.status(400).send({
             message: "user not found"
         });
         return;
     }
-    const dataQuote = {
+    
+    try {
+      
+      const dataQuote = {
         userId: user.id,
         status: 1,
         createdAt: Y+"-"+m+"-"+d,
         updatedAt: Y+"-"+m+"-"+d,
-    };
-
-    Quotes.create(dataQuote)
-    .then(data => {
-       result['quote'] = data; 
-      QuotesItems.create({
-        quoteId: data.id,
-        productId: req.body.productId,
-        product_name: req.body.productName,
-        price: product.price*req.body.quantity,
-        quantity: req.body.quantity
-      })
-      .then(item => {
-        result['item']=item;
-        res.send(result);
-      })
-      .catch(errs=>{
-        res.status(500).send({
-          message:
-            errs.message || "Some error occurred while creating the Quote."
-        });
-      });
-      
-    })
-    .catch(err => {
+      };
+      let quote = await Quotes.findOne({where :{ userId:payload.id}});
+      if (!quote) {
+        const quotes = await Quotes.create(dataQuote);
+        const dataItem = {
+          quoteId: quotes.id,
+          productId: req.body.productId,
+          product_name: req.body.productName,
+          price: product.price*req.body.quantity,
+          quantity: req.body.quantity
+        };
+        const items = await QuotesItems.create(dataItem);
+        result.push({quotes,items});
+      }
+      else
+      {
+        const dataItem = {
+          quoteId: quote.id,
+          productId: req.body.productId,
+          product_name: req.body.productName,
+          price: product.price*req.body.quantity,
+          quantity: req.body.quantity
+        };
+        const items = await QuotesItems.create(dataItem);
+        result.push({quote,items});
+      }
+      res.send(result);
+    } catch (err) {
       res.status(500).send({
         message:
           err.message || "Some error occurred while creating the Quote."
       });
-    });
+    }
 };
 
 // Retrieve all Products from the database.
-exports.findAll = (req, res) => {
-    let result = [];
-    Quotes.findAll({where:{ userId: req.userId }}).
-    then(data =>{
-      data.forEach((value) => {
-        const item = QuotesItems.findAll({where: {quoteId:value.id}});
-        result.push([value,item]);
-      });
+exports.findAll = async (req, res) => {
+  try {
+      const token = req.session.token;
+      const payload = jwt.decode(token, config.secret);
+
+      const quotes = await Quotes.findAll({ where: { userId: payload.id } });
+
+      const result = [];
+
+      for (const quote of quotes) {
+          const items = await QuotesItems.findAll({ where: { quoteId: quote.id } });
+          result.push({ quote, items });
+      }
+
       res.send(result);
-    }).catch(err => {
+  } catch (err) {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while get list transaction."
+          message: err.message || "Some error occurred while getting the list of transactions."
       });
-    });
+  }
 };
 
